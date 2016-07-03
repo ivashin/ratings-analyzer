2b1878bf28742630eaf83ad68580c4a50b99f7b2
Vasiliy Ivashin <Vasiliy Ivashin>
2016-07-03 13:56:19 +0300

    Added command-line parsing and logging

---
+++ src/RatingsAnalyzer/RatingsAnalyzer/CommandLineOptions.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+
+namespace RatingsAnalyzer
+{
+    class CommandLineOptions
+    {
+        [VerbOption("get", HelpText = "Extract data from an online source.")]
+        public GetOptions GetOptions { get; set; }
+
+        [VerbOption("analyze", HelpText = "Analyze extracted data.")]
+        public AnalyzeOptions AnalyzeOptions { get; set; }
+
+        [Option('s', "source", DefaultValue = Source.None, HelpText = "Data source")]
+        public Source Source { get; set; }
+
+        [Option('r', "results", DefaultValue = 100, HelpText = "Number of results to return")]
+        public int Results { get; set; }
+    }
+
+    class GetOptions
+    {
+        
+    }
+
+    class AnalyzeOptions
+    {
+        
+    }
+
+    enum Command
+    {
+        None,
+        Get,
+        Analyze
+    }
+
+    enum Source
+    {
+        None,
+        Metacritic,
+        RottenTomatoes,
+        Kinopoisk
+    }
+}
--- src/RatingsAnalyzer/RatingsAnalyzer/Runner.cs
+++ src/RatingsAnalyzer/RatingsAnalyzer/Runner.cs
@@ -1,9 +1,51 @@
-﻿namespace RatingsAnalyzer
+﻿using System;
+using CommandLine;
+using NLog;
+
+namespace RatingsAnalyzer
 {
     class Runner
     {
-        static void Main(string[] args)
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        static int Main(string[] args)
         {
+            var runner = new Runner();
+            return runner.Run(args);
+        }
+
+        public int Run(string[] args)
+        {
+            try
+            {
+                Logger.Info("Starting application");
+                Logger.Debug("Command line: {0}", String.Join(" ", args));
+
+                var options = new CommandLineOptions();
+                var command = Command.None;
+                if (!Parser.Default.ParseArguments(args, options, (verb, _) => Enum.TryParse(verb, out command))
+                    || command == Command.None)
+                {
+                    Logger.Error("Invalid command-line arguments");
+                    var help = CommandLine.Text.HelpText.AutoBuild(options);
+                    Console.WriteLine(help.ToString());
+                    return 1;
+                }
+
+                switch (command)
+                {
+                    case Command.Get:
+                        break;
+                    case Command.Analyze:
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e, "Critical error has occurred.");
+                return -1;
+            }
+            return 0;
         }
     }
 }
